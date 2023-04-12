6472c2e865d492043ab78e0118ecc1ca564a9822
MANTRI MOUNIKA <mounikamantri12@gmail.com>
2023-04-12 15:47:02 +0530

    remove commented code

--- SecurityProj/java/Test4.java
+++ SecurityProj/java/Test4.java
@@ -2,6 +2,6 @@ class Test4
 {
     public static void main(String []args)
     {
-        //System.out.println("My First Java Program.");
+        System.out.println("My First Java Program.");
     }
 };
